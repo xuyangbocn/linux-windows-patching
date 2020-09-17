7e8655eda9df3ca3647186021382c66084704c06
xuyangbocn <xuyangbocn@users.noreply.github.com>
2020-09-17 16:15:51 +0800

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,6 @@
 default['linux_patching']['var_partition_min_diskspace_required_in_GB'] = 0
 
+default['windows']['updates'] = [
+#    {'kb': 'KB4576750',
+#     'source': 'http://download.windowsupdate.com/d/msdownload/update/software/secu/2020/08/windows10.0-kb4576750-x64_c4e0b5e0f0835db971a40058aa17ae9a0d2f1e2a.msu'}
+]
